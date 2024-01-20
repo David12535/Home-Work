651bed6e0c90c1d1c2bec41bcdee69c8297a5b09
David12535 <davidberk28@gmail.com>
2024-01-20 20:48:52 +0400

    commit

---
+++ day 1/World.py
@@ -0,0 +1,12 @@
+name = "David"
+surname = "Berkacashvili"
+year= "2006"
+text = "aseve var youtuberi da aseve vapireb rom viyo momavalshi programisti "
+speak = "me var 17 wlis"
+fro= "var shida qartlidan kerzod goris raioni sofeli varianidan"
+print(name + " " + surname)
+print(speak + " " + text)
+print(fro)
+
+
+

0f440d206219642e9bfc77768aa6c47fbdaf2a80
David <davidberk28@gmail.com>
2024-02-13 16:29:36 +0400

    commit

---
+++ sacdeli/sacdeli 02.py
@@ -0,0 +1,23 @@
+thistuple = ("low", "medium", "high")
+print(thistuple)
+x = ("low", "medium", "high")
+y = list(x)
+y[2] = "ultra"
+x = tuple(y)
+print(x)
+thisdict = {"food": "water", "bed": "sleep", "year": 1981}
+x = thisdict["food"]
+username = input("David")
+print ("berkacashvili: " + "David")
+import pandas as pd
+a=[1, 7, 2]
+youare = pd.series(a)
+print(youare)
+
+
+
+
+
+
+
+

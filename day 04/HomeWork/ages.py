5eef2831d57a95517ba5e933bf6912ecb32073e4
David <davidberk28@gmail.com>
2024-02-14 23:25:20 +0400

    commit

---
+++ day 04/HomeWork/ages.py
@@ -0,0 +1,16 @@
+me = 17
+bro = 24
+mom = 40
+sister = 26
+#in 10 years
+me = me + 10
+me=str(me)
+bro = bro + 10
+bro=str(bro)
+mom = mom + 10
+mom=str(mom)
+sister=str(sister)
+print("ჩემი  ასაკი 10 წელ ში - " + me)
+print("ძმის  ასაკი 10 წელ ში - " + bro)
+print("დ ე დ ის  ასაკი 10 წელ ში - " + mom)
+print("დ ის  ასაკი 10 წელ ში - " + sister)
---
+++ day 04/classwork/sololearn.py
@@ -0,0 +1,29 @@
+#Declaring payment variables
+currency = "USD"
+amount = 200
+status = "overdue"
+
+#Displaying status
+print(status)
+
+#print("Game Over") 
+
+credit = 814
+Credit = 428
+CREDIT = 171
+
+#snakecase hello world
+
+salary = 728
+new_salary = salary + 200
+print(new_salary)
+
+salary = 512
+pay_raise = 81
+new_salary = salary + pay_raise
+print(new_salary)
+
+a = 20
+a = 40
+a = 80
+print(a)

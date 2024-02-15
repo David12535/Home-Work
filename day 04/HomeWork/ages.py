aebd8505f97195e94d264c512346b79ee99607d6
David <davidberk28@gmail.com>
2024-02-15 20:13:47 +0400

    commit

--- day 04/HomeWork/ages.py
+++ day 04/HomeWork/ages.py
@@ -2,15 +2,14 @@ me = 17
 bro = 24
 mom = 40
 sister = 26
+
 #in 10 years
-me = me + 10
-me=str(me)
-bro = bro + 10
-bro=str(bro)
-mom = mom + 10
-mom=str(mom)
-sister=str(sister)
-print("ჩემი  ასაკი 10 წელ ში - " + me)
-print("ძმის  ასაკი 10 წელ ში - " + bro)
-print("დ ე დ ის  ასაკი 10 წელ ში - " + mom)
-print("დ ის  ასაკი 10 წელ ში - " + sister)
+me += 10
+bro += 10
+mom += 10
+sister += 10
+
+print(f"ჩემი  ასაკი 10 წელ ში - {me}")
+print(f"ძმის  ასაკი 10 წელ ში - {bro}")
+print(f"დ ედ ის  ასაკი 10 წელ ში - {mom}")
+print(f"დ ის  ასაკი 10 წელ ში - {sister}")

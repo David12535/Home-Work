e80bf620f12280886bbc27cbe239f4a6333e7b96
David <davidberk28@gmail.com>
2024-02-16 21:17:07 +0400

    commit

---
+++ day 04/HomeWork/ourinfo.py
@@ -0,0 +1,12 @@
+name= ("David")
+surname = ("berkacashvili")
+height = ("1.80")
+age = ("17")
+#the best leader Data Tezelava
+#the best academy is Goa
+#:) :) :) 
+print (name + " " + surname)
+print (f"ჩემი სომაღ ლ ეა{height}")
+print(f"ჩემი ასაკია{age}")
+print()
+

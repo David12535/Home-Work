1dab398e2a9a747219feacd35871da3562b5defc
David <davidberk28@gmail.com>
2024-02-18 03:09:04 +0400

    commit

--- day 05/homework/book_prices.py
+++ day 05/homework/book_prices.py
@@ -12,15 +12,15 @@
 
 #Declare variables for ten types of book prices
 price_The_Hound_of_the_Baskervilles = 150
-price_a_tigger_skinner = 150
-price_carlson = 150
-price_spiral = 150
-price_me_grandmother_and_hilarion = 150
-price_READER_I_MURDERED_HIM = 150
-price_thirteen_dorways_wolves_behind_them_all = 150
-price_one_of_us_is_lying = 150
-price_the_heart_is_a_lo0nely_hunter = 150
-price_how_the_dead_speak = 150
+price_a_tigger_skinner = 371
+price_carlson = 41
+price_spiral = 73
+price_me_grandmother_and_hilarion = 97
+price_READER_I_MURDERED_HIM = 168
+price_thirteen_dorways_wolves_behind_them_all = 125
+price_one_of_us_is_lying = 65
+price_the_heart_is_a_lonely_hunter = 215
+price_how_the_dead_speak = 37
 
 #discounte of 10%
 discount01 = 10
@@ -30,12 +30,16 @@ discount04 = 10
 discount05 = 10
 
 #adding discounts01 -10%
-price_after_discount01 = price_The_Hound_of_the_Baskervilles - price_a_tigger_skinner * discount01 / 150
+price_after_discount01 = price_The_Hound_of_the_Baskervilles / discount01 
 print(price_after_discount01)
-price_after_discount02 = price_carlson - price_spiral * discount02 / 150
+price_after_discount02 = price_carlson  / discount02 
 print(price_after_discount02)
-price_after_discount03 = price_me_grandmother_and_hilarion - price_a_tigger_skinner * discount03 / 150
+price_after_discount03 = price_a_tigger_skinner / discount03 
 print(price_after_discount03)
+price_after_discount04 = price_me_grandmother_and_hilarion - price_a_tigger_skinner / discount03 
+print(price_after_discount04)
+price_after_discount05 = price_spiral / discount05 
+print(price_after_discount05)
 
 #discounte of 20%
 discount06 = 20
@@ -45,15 +49,15 @@ discount09 = 20
 discount10 = 20
 
 #adding discount02 -20%
-price_after_discount1 = price_READER_I_MURDERED_HIM - price_thirteen_dorways_wolves_behind_them_all * discount06 / 150
+price_after_discount1 = price_READER_I_MURDERED_HIM / discount06 
 print(price_after_discount1)
-price_after_discount2 = price_one_of_us_is_lying - price_the_heart_is_a_lo0nely_hunter * discount07 / 150
+price_after_discount2 = price_one_of_us_is_lying  / discount07 
 print(price_after_discount2)
-price_after_discount3 = price_one_of_us_is_lying - price_how_the_dead_speak * discount08 / 150
+price_after_discount3 = price_thirteen_dorways_wolves_behind_them_all  / discount08 
 print(price_after_discount3)
-
-
-
+price_after_discount4 = price_me_grandmother_and_hilarion / discount09
+print(price_after_discount4)
+price_after_discount5 = price_the_heart_is_a_lonely_hunter / discount10
 
 
 

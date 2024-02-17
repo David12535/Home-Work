9732be9f6301f74f469bbe3bbc031ae822c40b42
David <davidberk28@gmail.com>
2024-02-17 19:04:06 +0400

    commit

---
+++ day 05/classwork/book_price.py
@@ -0,0 +1,11 @@
+#book price
+book_price = 19
+#book price next
+book_free = 14
+print(f"wignis fasi manamde{book_price}")
+print(f"wignis fasi gaxda{book_free}")
+
+
+
+
+
---
+++ day 05/homework/book.py
@@ -0,0 +1,17 @@
+#Declaring variables for two types of books
+history_book = 5
+mathematics_book = 5
+
+#Declare variables for two types of book prices
+history_price = 28
+mathematics_price = 42
+
+#adding discounts
+history_price -= 2.8
+mathematics_price -= 8.4
+
+#displaying last prices
+print(history_price)
+print(mathematics_price)
+
+

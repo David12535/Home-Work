698e14cba1a6e23a5f65231ef3b61c83540b7514
David <davidberk28@gmail.com>
2024-03-08 21:06:55 +0400

    commit

---
+++ W3/sacdeli 06.py
@@ -0,0 +1,20 @@
+#The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+a = 77
+b = 77
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+
+#ExampleGet your own Python Server
+#Print all numbers from 0 to 5, and print a message when the loop has ended:
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+#
+

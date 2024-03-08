5d234c69d69f08bf8e18cb6a8486e4202e36d8b0
David <davidberk28@gmail.com>
2024-03-08 22:11:52 +0400

    commit

--- day 11/classwork/day11.py
+++ day 11/classwork/day11.py
@@ -2,5 +2,6 @@ balance = int(input("please enter your Amount of money: "))
 num2 = 100
 if balance >= num2:
         print("You have enough money: ")
-elif num2 >= balance:
+else:
+        num2 >= balance
         print("you don't have enough money: ")

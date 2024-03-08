722563a5b1819c07f6d2e77afccfc995e379649e
David <davidberk28@gmail.com>
2024-03-08 21:19:17 +0400

    commit

--- W3/sacdeli 06.py
+++ W3/sacdeli 06.py
@@ -16,5 +16,12 @@ for x in range(6):
 else:
   print("Finally finished!")
 
-#
+#Booleans represent one of two values: True or False.
+# In programming you often need to know if an expression is True or False.
+# You can evaluate any expression in Python, and get one of two answers, True or False.
+# When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
 

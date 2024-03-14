5022ff92ebd61a32d2a1e5b94581771ad78e4cb0
David <davidberk28@gmail.com>
2024-03-14 18:48:03 +0400

    commit

--- day 16/classwork/classwork16.py
+++ day 16/classwork/classwork16.py
@@ -21,3 +21,25 @@
 # function(5)
 # function(17)
 # function(18)
+
+# def add(num1, num2):
+#     return num1 + num2
+
+# def subtract(num1, num2):
+#     return num1 - num2
+
+# def multiply(num1, num2):
+#     return num1 * num2
+
+# def divide(num1, num2):
+#     return num1 / num2
+
+# print(add(5,5))
+# print(subtract(5,5))
+# print(multiply(5,5))
+# print(divide(5,5))
+
+
+
+
+

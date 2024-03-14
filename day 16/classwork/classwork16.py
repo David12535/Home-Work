de5a8745946c53494b5f6afcd8278673d9a721b7
David <davidberk28@gmail.com>
2024-03-14 18:23:11 +0400

    commit

--- day 16/classwork/classwork16.py
+++ day 16/classwork/classwork16.py
@@ -1,9 +1,23 @@
-def costumer_number(number):
-    print(number * 3)
-num1 = int(input("enter your number: "))
-if num1 != int:
-    print("it's False")
-else:
-    num1 == int
-    print("It's True")
-print("nika")
+# def costumer_number(number):
+#     print(number * 3)
+# num1 = int(input("enter your number: "))
+# if num1 != int:
+#     print("it's False")
+# else:
+#     num1 == int
+#     print("It's True")
+# print("nika")
+
+# def function(name):
+#         print(name)
+
+# function("akaki")
+# function("zaqro")
+# function("loti vano")
+
+# def function(integer):
+#         for i in range(integer):
+#                 print(i)
+# function(5)
+# function(17)
+# function(18)

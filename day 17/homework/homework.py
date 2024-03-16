b9aeea2231a0884877cab86cceb899a3fcf5b2e3
David <davidberk28@gmail.com>
2024-03-16 17:48:40 +0400

    commit

--- day 17/homework/homework.py
+++ day 17/homework/homework.py
@@ -43,13 +43,50 @@ def largest_number(numbers):
             max_number=num
             return max(numbers)
 
-print(largest_number([1,2,3,4,5,6,7,8,9,10]))
+print(largest_number([12,24,17,4,5,27,7,8,9,40]))
 
 #5
+def filter_list(strings_list):
+    filtered_list = []
 
+    for word in strings_list:
+        if word[0] == 'a':
+            filtered_list.append(word)
 
+    return filtered_list
 
+words = ["anakonda", "majlajuna", "pitoni", "ajganda", "anuki=anakonda"]
+print(filter_list(words))
 
+#6
+def squared_numbers(numbers):
+    squared_numbers_list = []
 
+    for num in numbers:
+        squared_numbers_list.append(num * num)
 
+    return squared_numbers_list
 
+print(squared_numbers([1,2,3,4,5]))
+
+#7
+def strings_length(strings_list):
+    length_list = []
+ 
+    for word in strings_list:
+        length_list.append(len(word))
+
+    return length_list
+print(strings_length(["maria,", "saba", "luka", "oqroa", "namcxvari"]))
+
+#8
+def sum_of_numbers(numbers):
+    result = 0
+
+    for num in numbers:
+        if num > 10:
+            result = result + num
+    
+    return result
+
+print(sum_of_numbers([1,2,3,11,15,5]))

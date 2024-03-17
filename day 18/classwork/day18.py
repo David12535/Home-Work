8ea9fe6c5d502afe2b293469d50fe29f78d2b221
David <davidberk28@gmail.com>
2024-03-17 19:11:21 +0400

    commit

--- day 18/classwork/day18.py
+++ day 18/classwork/day18.py
@@ -1,5 +1,31 @@
-num1 = int(input("Please enter int: "))
-factorial_result = 1
-my_list = []
-for i in range(num1 + 1, 2):
-    my_list.append(i)
+# დავალება: შექმენით პროგრამა, სადაც მომხმარებელს შემოატანინებთ თუ რამდენ რიცხვს შეიტანენ სიაში. შემდეგ შექმენით სია, for ციკლში input-ით
+#  შეეკითხეთ რიცხვი და შეიტანეთ ამ სიაში ეს რიცხვები. ბოლოს სიის ჯამი დააბრუნეთ 
+
+def filter_evens(numbers):
+    filtered_list = []
+
+    for num in numbers:
+        if num >= 10 and num % 2 == 0:
+            filtered_list.append(num)
+    
+    return filtered_list
+
+def sum_of_numbers(numbers):
+    sum = 0
+
+    for i in numbers:
+        sum = sum + i
+    
+    return sum
+
+count = int(input("Please enter how many numbers do you want to input: "))
+
+numbers = []
+
+for i in range(count):
+    number = int(input("Please enter number: "))
+    numbers.append(number)
+
+
+print(sum_of_numbers(numbers))
+print(filter_evens(numbers))
